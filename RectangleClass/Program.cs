1b91063dd52e33232ed7151ea3f64321f9eb5a78
unknown <nellysherova@mail.ru>
2021-01-27 21:51:57 +0300

    Создала класс Rectangle

--- RectangleClass/Program.cs
+++ RectangleClass/Program.cs
@@ -2,11 +2,32 @@
 
 namespace RectangleClass
 {
+    class Rectangle 
+    {
+        public double slide1;
+        public double slide2;
+        public Rectangle(){}
+        public Rectangle(double slide1, double slide2)
+        {
+            this.slide1 = slide1;
+            this.slide2 = slide2;
+        }
+        public double AreaCalculator()
+        {
+            return slide2 * slide1;
+        }
+        public double PerimeterCalculator()
+        {
+            return 2 * (slide1 + slide2);
+        }
+        public double Area {get{return AreaCalculator();}}
+        public double Perimeter{get{return PerimeterCalculator();}}
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
         }
     }
 }
